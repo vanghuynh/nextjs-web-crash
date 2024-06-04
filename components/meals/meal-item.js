c7bad6fea109e71e11c99af857f6a883cec4e499
Vang Huynh <huynh.vang@devexperts.com>
2024-06-04 14:32:57 +0700

    add sqlite and show list of meals

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -7,7 +7,7 @@ export default function MealItem({ title, slug, image, summary, creator }) {
     <article className={classes.meal}>
       <header>
         <div className={classes.image}>
-          <Image src={image} alt={title}></Image>
+          <Image src={image} alt={title} fill></Image>
         </div>
         <div className={classes.headerText}>
           <h2>{title}</h2>
