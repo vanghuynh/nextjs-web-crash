2f7ed04d8ecc939b6e22554c503786efbb16850d
Vang Huynh <huynh.vang@devexperts.com>
2024-06-05 14:45:46 +0700

    handle for error and not found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, this page does not exist, please check the URL</p>
+    </main>
+  );
+}
