9b4f7cac858e380219e14f8bfaf7f0caec6e3ed3
Vang Huynh <huynh.vang@devexperts.com>
2024-06-05 14:32:34 +0700

    add loading feature

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,14 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "../../components/meals/meals-grid";
 import { getMeals } from "../../lib/meals";
-export default async function MealsPage() {
+import { Suspense } from "react";
+
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +23,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
