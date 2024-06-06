87570a3ed7b681c534c50f98d596a7dbe1492f59
Vang Huynh <huynh.vang@devexperts.com>
2024-06-06 08:27:26 +0700

    meal detail and not found page

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not found</h1>
+      <p>Unfortunately, this page does not exist, or meal not exist.</p>
+    </main>
+  );
+}
